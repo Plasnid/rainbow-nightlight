4b4f3f0658e58dde857f7d352952d83bc94589dd
Stuart Sackler <stuart@sackler.ca>
2020-11-15 21:44:06 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,38 @@
-let ring = neopixel.create(DigitalPin.P2, 7, NeoPixelMode.RGB)
-ring.setBrightness(10)
-ring.showRainbow(1, 360)
-basic.forever(function () {
-    ring.show()
-    ring.rotate(1)
-    basic.pause(500)
+let aPressed = false;
+let bPressed = false;
+let ring= neopixel.create(DigitalPin.P2, 7, NeoPixelMode.RGB)
+
+input.onButtonPressed(Button.A, function () {
+    if (aPressed == false) {
+        aPressed = true
+    } else {
+        aPressed = false
+    }
+});
+input.onButtonPressed(Button.B, function () {
+   if(bPressed==false){
+       ring.clear();
+       bPressed = true;
+   }else{
+       startRainbow();
+       bPressed = false;
+   }
 })
+function startRainbow(){
+    aPressed = false
+    ring = neopixel.create(DigitalPin.P2, 7, NeoPixelMode.RGB)
+    ring.setBrightness(20)
+    ring.showRainbow(1, 360)
+}
+basic.forever(function () {
+    if (aPressed == false) {
+        ring.show()
+        ring.rotate(1)
+        basic.pause(100)
+    } else {
+        ring.show()
+        ring.rotate(-1)
+        basic.pause(100)
+    }
+})
+startRainbow();
